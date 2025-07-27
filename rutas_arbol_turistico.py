ecf50513421533092066fdb327e1d661b6763d57
Lizbeth593 <lizbeth59312@gmail.com>
2025-07-27 13:28:01 -0500

    Ruta_turistica_arbol
    Arbol binario de los lugares turisticos

---
+++ rutas_arbol_turistico.py
@@ -0,0 +1,21 @@
+arbol_turistico = {
+    "valor": "Mitad del Mundo (Sierra)",
+    "izquierda": {
+        "valor": "Parque Nacional Galápagos (Insular)",
+        "izquierda": {
+            "valor": "Malecón 2000 (Costa)",
+            "izquierda": None,
+            "derecha": None
+        },
+        "derecha": {
+            "valor": "Baños de Agua Santa (Sierra)",
+            "izquierda": None,
+            "derecha": None
+        }
+    },
+    "derecha": {
+        "valor": "Parque Yasuni (Amazonía)",
+        "izquierda": None,
+        "derecha": None
+    }
+}
